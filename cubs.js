a83c8060f88d3cf82423abe61458eb2fa005fbfc
Daniil <daniel217032001@gmail.com>
2018-04-08 16:13:08 +0300

    Start

---
+++ cubs.js
@@ -0,0 +1,108 @@
+let cubes = function(parent, settings) {
+  if(parent.id==='') return;
+  const wNum = settings.columns || 10,
+    hNum = settings.rows || 10,
+    wCub = parent.offsetWidth / wNum,
+    hCub = parent.offsetHeight / hNum || wCub,
+    speed = settings.speed || 100,
+    transition = settings.transition + 'ms' || '1000ms';
+  //generate grid
+  {
+    const color = settings.color || 'rgba(66, 134, 244, 1)';
+    let html = '<div class="cubs">';
+    for (let i = 0; i < hNum; i++) {
+      html = html + ` <div class="cubes-row" data-cubes="${i}">`
+      for (let j = 0; j < wNum; j++) {
+        html = html + ` <div class="cubes-block cubes-block__base" data-cubes="${j}"></div>`
+      }
+      html = html + `</div>`
+    }
+    html = html + `</div>`
+    parent.innerHTML = parent.innerHTML + html;
+
+    parent.style.position = 'relative';
+    const css = `
+    #${parent.id} .cubs {
+      position: absolute;
+      top:0;
+      left:0;
+    }
+    #${parent.id} .cubes-row {
+      height: ${hCub}px;
+      position: relative;
+    }
+
+    #${parent.id} .cubes-block {
+      width: ${wCub}px;
+       height: ${hCub}px;
+      position: absolute;
+      transition: ${transition};
+    }
+
+    #${parent.id} .cubes-block__base {
+      background-color: ${color};
+    }
+
+    #${parent.id} .cubes-block__toggled {
+      background-color: rgba(0, 0, 0, 0);
+    }`,
+      head = document.head || document.getElementsByTagName('head')[0],
+      style = document.createElement('style');
+
+    style.type = 'text/css';
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    head.appendChild(style);
+  }
+
+  let allblocks = Array.from(parent.getElementsByClassName("cubes-row")),
+    ammountAllBlocks = 0,
+    ammoutBlocks = 0,
+    toggleShow = true;
+
+  allblocks.forEach((block, index) => {
+    allblocks[index] = [];
+
+    Array.from(block.getElementsByClassName("cubes-block")).forEach((element) => {
+      ammountAllBlocks++;
+
+      allblocks[index][element.dataset.cubes] = element;
+      element.style.left = wCub * parseInt(element.dataset.cubes) + "px";
+
+      element.addEventListener("mouseenter", function() {
+          changeColor(parseInt(this.dataset.cubes), parseInt(this.parentNode.dataset.cubes));
+      });
+    });
+  });
+
+  let changeColor = (x, y) => {
+    let base, toggled;
+    if (toggleShow) {
+      base = "cubes-block__base";
+      toggled = "cubes-block__toggled";
+    } else {
+      base = "cubes-block__toggled";
+      toggled = "cubes-block__base";
+    }
+    setTimeout(() => {
+      let block = allblocks[y][x];
+      if (block !== undefined && !block.classList.contains(toggled)) {
+        ammoutBlocks++;
+        block.classList.toggle(base);
+        block.classList.toggle(toggled);
+        if (allblocks[y][x - 1] !== undefined) changeColor(x - 1, y);
+        if (allblocks[y][x + 1] !== undefined) changeColor(x + 1, y);
+        if (allblocks[y - 1] !== undefined) changeColor(x, y - 1);
+        if (allblocks[y + 1] !== undefined) changeColor(x, y + 1);
+        if (ammoutBlocks == ammountAllBlocks) {
+          ammoutBlocks = 0;
+          toggleShow = !toggleShow;
+        }
+      }
+    }, speed);
+  };
+};
---
+++ scripts.js
@@ -0,0 +1,23 @@
+cubes(document.getElementById("s1"), {
+  columns: 3,
+  rows: 6,
+  color: 'rgba(28, 71, 123)',
+  speed: 50,
+  transition: 2000
+});
+
+cubes(document.getElementById("s2"), {
+  columns: 35,
+  rows: 15,
+  color: 'rgba(87, 28, 122, 0.8)',
+  speed: 100,
+  transition: 1000
+});
+
+cubes(document.getElementById("s3"), {
+  columns: 25,
+  rows: 25,
+  color: 'rgba(28, 71, 123, 0.99)',
+  speed: 50,
+  transition: 2000
+});
